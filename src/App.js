e4dd6c61c393d2879425b1209c30cc28e37bab47
Mati ul Rehman <iammatiz00&gmail.com>
2022-07-04 00:28:03 +0500

    if else added;

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "Mati"; //variable for storing name
+  const isMale = true;
+
+  function getGreetings(user) {
+    if (user) {
+      return <h4>Hello, {user}!</h4>;
+    } else {
+      return <h5>Hello Stranger!</h5>;
+    }
+  }
   return (
     <div className="App">
-       <h1>Hello React!</h1><hr></hr>
-       <h2>~Matiz</h2>
+      <h1>{getGreetings("Matiz")}</h1>
+      <hr></hr>
+      <h2>~{name}</h2>
+      <h3>You are a {isMale ? "Male" : "Female"}</h3>
     </div>
   );
 }
