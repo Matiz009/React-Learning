0d5b7803dafd0b6e9911199dcee5f9dc2aaf308c
Mati ul Rehman <iammatiz00&gmail.com>
2022-07-19 01:52:04 +0500

    Todo app;

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from "react";
-import Product from "./Product";
+import { useState } from "react";
+import "./App.css";
 function App() {
-  return <Product name="Apple" decription="Your AI Assistant" price={59.9} />;
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+  const addTodo = (e) => {
+    e.preventDefault(); //this prevents the refresh
+    setTodos([...todos, input]);
+    setInput("");
+    e.append(<h3>List of elements </h3>);
+  };
+  return (
+    <div className="app">
+      <h1 className="main">Welcome to my TODO List</h1>
+      <hr></hr>
+      <form className="form">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+        />
+        <button type="submit" onClick={addTodo}>
+          Add To Do
+        </button>
+      </form>
+
+      <h2 className="d1">List of Todos</h2>
+      {todos.map((todo) => (
+        <p>{todo}</p>
+      ))}
+    </div>
+  );
 }
 
 export default App;
