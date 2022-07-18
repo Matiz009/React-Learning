1fc3c97f340c548d4494b1cf5b5103ec6f75dfbc
Mati ul Rehman <iammatiz00&gmail.com>
2022-07-19 01:10:05 +0500

    styles added;

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./Product.css";
 import Item_Dec from "./Item_Dec";
 function Product({ name, description, price }) {
   return (
-    <>
+    <div className="product">
       <Item_Dec name={name} description={description} />
-    </>
+      <h2 className="product__error">An error occurred</h2>
+    </div>
   );
 }
 export default Product;
