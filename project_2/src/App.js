4d89e36308f68c0960e2659283925d84f31e5d60
MATI UL REHMAN <iammatiz00@gmail.com>
2023-08-08 23:52:19 +0500

    project 2 almost completed

--- project_2/src/App.js
+++ project_2/src/App.js
@@ -1,11 +1,18 @@
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Videos from './components/Videos';
 
 function App() {
   return (
     <Router>
       <Header />
-      <Routes> </Routes>{' '}
+      <Routes>
+        <Route path="/" element={<Home />} />{' '}
+        <Route path="/videos" element={<Videos />} />{' '}
+      </Routes>{' '}
+      <Footer />
     </Router>
   );
 }
