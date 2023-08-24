a734499827f33812fa621e580d84065eee7fed00
MATI UL REHMAN <iammatiz00@gmail.com>
2023-08-24 19:51:01 +0500

    crd completed

--- project-6/src/Chapters/Chapter2.jsx
+++ project-6/src/Chapters/Chapter2.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Video from "../components/Video";
 import data from "../data/data";
 
 const Chapter2 = () => {
+  const [videos, setVideos] = useState(data);
+
+  const addHandler = () => {
+    const count = videos.length + 1;
+    const newVideo = {
+      title: `Channel ${count}`,
+      url: "https://picsum.photos/200/300?whitescale",
+      verified: true,
+    };
+
+    setVideos([...videos, newVideo]);
+  };
+
+  const delHandler = (index) => {
+    const updatedVideos = [...videos];
+    updatedVideos.splice(index, 1);
+    setVideos(updatedVideos);
+  };
+
   return (
     <div>
-      {data.map((item, index) => (
+      <button onClick={addHandler}> Add Video </button>{" "}
+      <button onClick={() => delHandler(videos.length - 1)}>
+        {" "}
+        Delete Last Video{" "}
+      </button>{" "}
+      {videos.map((item, index) => (
         <div key={index}>
-          <Video title={item.title} url={item.url} verified={item.verified} />
+          <Video title={item.title} url={item.url} verified={item.verified} />{" "}
           <br />
         </div>
-      ))}
+      ))}{" "}
     </div>
   );
 };
--- project-6/src/components/Video.jsx
+++ project-6/src/components/Video.jsx
@@ -1,32 +1,48 @@
+// Import required modules and components
 import React, { useState } from "react";
-import "./Video.css";
-import PlayButton from "./PlayButton";
+import "./Video.css"; // Import the CSS file for styling
+import PlayButton from "./PlayButton"; // Import the PlayButton component
 
+// Define the Video component that takes props: url, title, verified
 const Video = ({ url, title, verified }) => {
-  const [likes, setLikes] = useState(0); // Initialize likes state for each video
+  // Initialize the likes state for each video using useState hook
+  const [likes, setLikes] = useState(0);
+
+  // Click handler for the like button
+  const handleBtn = () => {
+    // Increment the likes count by 1 and update the state
+    setLikes(likes + 1);
+    console.log(likes); // Log the current likes count to the console
+  };
 
   return (
     <div className="video-container">
-      <img src={url} alt="random" className="video-image" />
+      {" "}
+      {/* Display the video thumbnail */}{" "}
+      <img src={url} alt="random" className="video-image" />{" "}
+      {/* Display the video's title */}{" "}
       <h3 className={`video-title `}> {title} </h3>{" "}
+      {/* Display verification status with a checkmark or "X" mark */}{" "}
       <p className="video-status"> Status: {verified ? "✔️" : "❌"} </p>{" "}
       <div>
         {" "}
+        {/* Render a PlayButton with a message and type prop */}{" "}
         <PlayButton message={"Channel is updating"} type="Alert">
           {" "}
           Alert{" "}
         </PlayButton>{" "}
+        {/* Render another PlayButton with a different message and type prop */}{" "}
         <PlayButton message={"Hi Youtube"} type="Console">
           {" "}
           Console{" "}
         </PlayButton>{" "}
       </div>{" "}
-      <br />
-      <button className="like-button" onClick={() => setLikes(likes + 1)}>
+      <br /> {/* Render the like button with an onClick event */}{" "}
+      <button className="like-button" onClick={handleBtn}>
         {" "}
         ❤️{" "}
       </button>{" "}
-      <p> Number of Likes: {likes} </p>{" "}
+      {/* Display the number of likes */} <p> Number of Likes: {likes} </p>{" "}
     </div>
   );
 };
